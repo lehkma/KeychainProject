bb928a19626bdd1881c04c3d7ece4074a58fbeb9
lehkma <deermartinal@gmail.com>
2022-08-07 14:56:14 +0200

    little changes

--- KeychainProject/MainForm.h
+++ KeychainProject/MainForm.h
@@ -8,6 +8,7 @@
 #include <json/json.h>
 #include "AddNewDataForm.h"
 #include "ViewingForm.h"
+//#include "ProfileForm.h"
 
 namespace KeychainProject {
 
@@ -417,6 +418,8 @@ private: System::Void label_Click(System::Object^ sender, System::EventArgs^ e) 
 	viewingForm->ShowDialog();
 }
 private: System::Void picProfile_Click(System::Object^ sender, System::EventArgs^ e) {
+	//ProfileForm^ profileForm = gcnew ProfileForm(user);
+	//profileForm->ShowDialog();
 }
 };
 }
