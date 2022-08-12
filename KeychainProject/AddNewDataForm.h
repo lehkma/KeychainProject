b8dfdfd0a271bb9d44c911adc6e55788491c611d
lehkma <deermartinal@gmail.com>
2022-08-12 14:27:26 +0200

    tried some chsnges

--- KeychainProject/AddNewDataForm.h
+++ KeychainProject/AddNewDataForm.h
@@ -184,7 +184,7 @@ namespace KeychainProject {
 			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
 				85.94891F)));
 			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Absolute,
-				180)));
+				181)));
 			this->tableLayoutPanel1->Controls->Add(this->btOK, 2, 0);
 			this->tableLayoutPanel1->Controls->Add(this->labelAdd, 0, 0);
 			this->tableLayoutPanel1->Controls->Add(this->comboBoxAdd, 1, 0);
@@ -201,9 +201,9 @@ namespace KeychainProject {
 				| System::Windows::Forms::AnchorStyles::Right));
 			this->btOK->Font = (gcnew System::Drawing::Font(L"Rubik", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->btOK->Location = System::Drawing::Point(539, 3);
+			this->btOK->Location = System::Drawing::Point(538, 3);
 			this->btOK->Name = L"btOK";
-			this->btOK->Size = System::Drawing::Size(175, 45);
+			this->btOK->Size = System::Drawing::Size(176, 45);
 			this->btOK->TabIndex = 5;
 			this->btOK->Text = L"OK";
 			this->btOK->UseVisualStyleBackColor = true;
@@ -234,7 +234,7 @@ namespace KeychainProject {
 			this->comboBoxAdd->ItemHeight = 26;
 			this->comboBoxAdd->Location = System::Drawing::Point(78, 9);
 			this->comboBoxAdd->Name = L"comboBoxAdd";
-			this->comboBoxAdd->Size = System::Drawing::Size(455, 34);
+			this->comboBoxAdd->Size = System::Drawing::Size(454, 34);
 			this->comboBoxAdd->TabIndex = 2;
 			// 
 			// tableLayoutPanel4
